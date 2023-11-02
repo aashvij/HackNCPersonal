1f5ec00de24eb7c9a5e37053bd6ca88089cf7bca
Aashvi Jain <aashvijain@aashvis-air.wireless-1x.unc.edu>
2023-11-02 18:55:06 -0400

    more changes

--- jumpypig.js
+++ jumpypig.js
@@ -2,24 +2,34 @@
 let board;
 let boardWidth = 360;
 let boardHeight = 640;
-let context; 
+let context;
 
 // pig variables 
-let pigWidth = 40;
-let pigHeight = 30;
+let pigWidth = 90;
+let pigHeight = 90;
 let pigY = boardHeight/2;
 let pigX = boardWidth/8;
 
 // pig object
-let pig = { x : pigX, y : pigY, height : pigHeight, width : pigWidth };
+let pig = { x : pigX, y : pigY, width : pigWidth, height : pigHeight };
 
 // onload function
 window.onload = function(){
     board = document.getElementById("board");
     board.width = boardWidth;
     board.height = boardHeight;
-    context = board.getContext("2d"); // 2d, 2dimensional context
+    context = board.getContext('2d'); // 2d, 2dimensional context
+
+    // pig icon
+    pigImage = new Image();
+    pigImage.src = "./pig.png";
+    pigImage.onload = function(){
+        context.drawImage(pigImage, pig.x, pig.y, pig.width, pig.height);
+    }
+
+    requestAnimationFrame(redraw);
 }
 
-// pig design
-
+function redraw(){
+    context.clearRect(0, 0, board.width, board.height);
+}
